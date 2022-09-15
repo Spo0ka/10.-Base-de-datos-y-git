108705cd452ec34c5e4e913ac8058bf93084ebf1
Salvador <Spo0ka@hotmail.com>
2022-09-15 18:32:31 -0500

    Se creo el esqueleto del proyecto

---
+++ EntidadesTienda/Tienda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesTienda
+{
+    public class Tienda
+    {
+        public Tienda(int idproducto, string nombre, string descripcion, double precio)
+        {
+            Idproducto = idproducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public int Idproducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
