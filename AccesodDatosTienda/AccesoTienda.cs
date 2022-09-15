9ea28516d6023e05087be79957c685e52d8f8257
Salvador <Spo0ka@hotmail.com>
2022-09-15 18:36:45 -0500

    Se agrego el acceso a datos para conectar con la base de datos

--- AccesodDatosTienda/AccesoTienda.cs
+++ AccesodDatosTienda/AccesoTienda.cs
@@ -10,6 +10,20 @@ namespace AccesodDatosTienda
 {
     public class AccesoTienda
     {
-        
+        Base b = new Base("Localhost", "Root", "", "Tienda");
+
+        public void Guardar(dynamic Entidad)
+        {
+            b.comando(String.Format("call insertarProducto({0},'{1}','{2}',{3})", Entidad.Idproducto,
+                Entidad.Nombre, Entidad.Descripcion, Entidad.Precio));
+        }
+        public DataSet Mostrar(dynamic Entidad)
+        {
+            return b.Obtener(String.Format("call showproducto('{0}')", Entidad.Nombre), "Producto");
+        }
+        public void Eliminar(dynamic Entidad)
+        {
+            b.comando(String.Format("call deleteproducto({0})", Entidad.Idproducto));
+        }
     }
 }
--- ManejadorTienda/ManejadorTienda.cs
+++ ManejadorTienda/ManejadorTienda.cs
@@ -12,6 +12,8 @@ namespace ManejadorTienda
 {
     public class ManejadorTienda
     {
-       
+        
     }
+
+   
 }
